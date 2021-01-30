e5bc192484ae0e109ef631bfd8300bb86e96cc67
Leo Andres <lmandres@yahoo.com>
2021-01-30 13:48:14 -0500

    Add ability to pass site index to getLocation.

--- GGJTeamWhatever2021/scripts/page_scripts.js
+++ GGJTeamWhatever2021/scripts/page_scripts.js
@@ -3,10 +3,12 @@ let radius = 20;
 
 let myLocation;
 let local_data;
+let local_index;
 
-function getLocation(location_span, site_data) {
+function getLocation(location_span, site_data, site_index) {
 
     local_data = site_data;
+    local_index = site_index;
     myLocation = location_span;
 
     if (navigator.geolocation) {
@@ -21,6 +23,7 @@ function showPosition(position) {
     let distance = GreatCircle.distance(40.26392, -76.88929, position.coords.latitude, position.coords.longitude, "FT");
 
     console.log(local_data);
+    console.log(local_data[local_index]);
 
     if (distance < radius) {
         myLocation.innerHTML = "<a href=\"ymca.html\">Click Here</a>.";
