922234caf1cd769c3398bb59a4489e219ac43dcc
Leo Andres <lmandres@yahoo.com>
2021-01-30 13:02:50 -0500

    Testing removing assigning to site_name variable in JS.

--- GGJTeamWhatever2021/scripts/site_loader.js
+++ GGJTeamWhatever2021/scripts/site_loader.js
@@ -15,9 +15,9 @@ function draw() {
     // default to first site if none was specified
     site = params.site == null ? 0 : params.site;
 
-    site_name = select('#name').html(site_data["sites"][site]["site_name"]);
-    site_name = select('#description').html(site_data["sites"][site]["description"]);
-    site_name = select('#hint').html(site_data["sites"][site]["hint"]);
+    select('#name').html(site_data["sites"][site]["site_name"]);
+    select('#description').html(site_data["sites"][site]["description"]);
+    select('#hint').html(site_data["sites"][site]["hint"]);
 
 
 }
